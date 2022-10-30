d727034f536797bd0d18f24b6926e95b34626165
Cody <baicaihenxiao@gmail.com>
2022-10-30 23:50:55 +0800

    Update CRHClient.py

--- TDDP/CRHClient.py
+++ TDDP/CRHClient.py
@@ -1,7 +1,7 @@
 import numpy as np
 import re
 
-from truthdiscovery import MatrixDataset
+from truthdiscovery import MatrixDataset, DistanceMeasures, ConvergenceIterator
 from truthdiscovery.algorithm import CRH
 from parse_param import ParseParam
 
@@ -14,16 +14,27 @@ from parse_param import ParseParam
 
 mydata = MatrixDataset(np.array([
     [4, 7, 0],
-    [0, 7, 8],
+    [4, 7, 0],
+    [10, 7, 8],
     [3, 0, 5],
     [3, 6, 8]
 ]))
 
 parseParam = ParseParam()
-params = parseParam.get_param_dict('iterator=l1-convergence-0.0010000000-limit-100')
-print(params)
-crh = CRH(**params)
+# params = parseParam.get_param_dict('iterator=l1-convergence-0.0010000000-limit-100')
+# print(params)
+# crh = CRH(**params)
+# results = crh.run(mydata)
+# print(results)
+# pres = parseParam.get_output_obj(results)
+# print(pres)
+
+
+conIter = ConvergenceIterator(DistanceMeasures.L1, 0.001, limit=100)
+crh = CRH(conIter)
 results = crh.run(mydata)
 print(results)
 pres = parseParam.get_output_obj(results)
 print(pres)
+
+
